99fcdda9d4c461289f7638381805f906b0543f50
Alex Powell <apowel24@uwo.ca>
2019-06-06 15:12:12 -0400

    backend/authentication/jwtauthenticator: moved options into the method parameters, since options are different for both methods, added signing method

--- src/backend/authentication/controllers/JWTuthenticator.js
+++ src/backend/authentication/controllers/JWTuthenticator.js
@@ -7,27 +7,59 @@ class JWTAuthenticator extends TokenAuthenticator{
     /**
      * contructor, creates the authenticator
      * @param {String} algorithm 
+     * algorithm to use for signing and decoding
+     * 
      * @param {KeyHolder} publicKeyholder 
+     * holds onto the public key
+     * 
      * @param {KeyHolder} privateKeyholder 
+     * holds onto the private key
+     * 
      * @param {jsonwebtoken} jwt 
-     * @param {Object} options 
+     * jsonwebtoken library, allows for dependancy injection by passing in
      */
-    constructor(algorithm, publicKeyholder, privateKeyholder, jwt, options){
+    constructor(algorithm, publicKeyholder, privateKeyholder, jwt){
         super(algorithm);
         this.publicKey = publicKeyholder;
         this.privateKey = privateKeyholder;
         this.jwt = jwt;
-        this.options = options
-        options.algorithm = this.algorithm;
     }
     /**
      * decodes jwt token with the required options
      * @param {String} token 
+     * token string to decode
      */
-    decodeToken(token){
+    decodeToken(token, options){
+        if(!options.algorithm)options.algorithm = [this.algorithm];
+        else options.algorithm.push(this.algorithm);
         this.jwt.verify(token, await this.publicKey.getKey(), options, (err, decoded) => {
-            if(err)console.log(err);
-            else return decoded;
-        });
+            if(err){
+                console.log(err);
+                return null;
+            }
+            else {
+                return decoded;
+            }
+        });  
+    }
+    /**
+     * Signs a token with the specified options
+     * @param {Object} token 
+     * token string to decode
+     * 
+     * @param {Object} options 
+     * options for the token
+     */
+    signToken(token, options){
+        if(!options.algorithm)options.algorithm = this.algorithm;
+        this.jwt.sign(token, await this.privateKey.getKey(), options, (err, signed) => {
+            if(err){
+                console.log(err);
+                return null;
+            }
+            else {
+                return signed;
+            }
+        })
     }
 }
